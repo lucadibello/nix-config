858b9536507404d41eb686ef5d929d9acf48861d
Luca Di Bello <lucadibello@proton.me>
2025-12-06 00:57:24 +0100

    feat: now enabled sudo auth using TouchID and Watch

--- darwin/system.nix
+++ darwin/system.nix
@@ -1,6 +1,11 @@
 {
   # Add ability to used TouchID for sudo authentication
-  security.pam.services.sudo_local.touchIdAuth = true;
+  security.pam.services.sudo_local = {
+    enable = true;
+    reattach = true;
+    touchIdAuth = true;
+    watchIdAuth = true;
+  };
 
   # MacOS system settings
   system = {
