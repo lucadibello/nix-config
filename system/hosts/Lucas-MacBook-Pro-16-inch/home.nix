4ebef678bac469e07e4bc23dd053e53f05533d6b
Luca Di Bello <lucadibello@proton.me>
2025-12-05 01:20:37 +0100

    fix: removed kitty.conf reference

--- system/hosts/Lucas-MacBook-Pro-16-inch/home.nix
+++ system/hosts/Lucas-MacBook-Pro-16-inch/home.nix
@@ -19,12 +19,11 @@
   # Configuration files
   # xdg.configFile."fish/config.fish".source = ../../config/fish/config.fish;
   xdg.configFile."starship.toml".source = ../../config/starship.toml;
-  xdg.configFile."kitty/kitty.conf".source = ../../config/kitty/kitty.conf;
   xdg.configFile."ghostty/config".source = ../../config/ghostty/config;
   xdg.configFile."borders/bordersrc".source = ../../config/borders/bordersrc;
   xdg.configFile."zathura/zathurarc".source = ../../config/zathura/zathurarc;
   
-  # Map ghostty's tmux-attach.sh to where kitty expects it
+  # Map ghostty's tmux-attach.sh to where ghostty expects it
   xdg.configFile."tmux/tmux-attach.sh".source = ../../config/ghostty/tmux-attach.sh;
 
   home.file.".tmux.conf".source = ../../config/tmux.conf;
