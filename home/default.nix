7567f2e5dd38681ccc8fb31ae5403377f8aaa4cf
Luca Di Bello <lucadibello@proton.me>
2026-01-12 18:38:08 +0100

    feat: added opencode rules

--- home/default.nix
+++ home/default.nix
@@ -14,6 +14,7 @@
     ./ghostty.nix
     ./zed.nix
     ./karabiner.nix
+    ./opencode.nix
   ];
 
   home = {
---
+++ home/opencode.nix
@@ -0,0 +1,30 @@
+{
+  xdg.configFile."opencode/AGENTS.md".source = ../config/opencode/AGENTS.md;
+
+  programs.opencode = {
+    settings = {
+      autoupdate = true;
+      theme = "kanagawa";
+
+      mcp = {
+        # Docker MCP gateway configuration
+        "mcp_docker" = {
+          type = "local";
+          command = [
+            "docker"
+            "mcp"
+            "gateway"
+            "run"
+          ];
+          enabled = true;
+        };
+
+        # might be removed later is docker mcp actually works!
+        "context7" = {
+          type = "remote";
+          url = "https://mcp.context7.com/mcp";
+        };
+      };
+    };
+  };
+}
