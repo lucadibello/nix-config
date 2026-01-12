051eb21b846fe119557690615c4e147d21aed326
Luca Di Bello <lucadibello@proton.me>
2026-01-12 22:31:43 +0100

    fix: now `karabiner` home-manager script only executed in laptop (where `karabiner-elements` is installed)

--- home/default.nix
+++ home/default.nix
@@ -13,7 +13,6 @@
     ./fzf.nix
     ./ghostty.nix
     ./zed.nix
-    ./karabiner.nix
     ./opencode.nix
   ];
 
--- hosts/Lucas-MacBook-Pro-16-inch/configuration.nix
+++ hosts/Lucas-MacBook-Pro-16-inch/configuration.nix
@@ -1,7 +1,14 @@
+{ primaryUser, ... }:
 {
   networking.hostName = "Lucas-MacBook-Pro-16-inch";
 
   homebrew.casks = [
     "karabiner-elements"
   ];
+
+  home-manager.users.${primaryUser} = {
+    imports = [
+      ../../home/karabiner.nix
+    ];
+  };
 }
