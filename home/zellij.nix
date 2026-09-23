5b0d1f64128ef9a92a7e82cf7bdf0de423813164
Luca Di Bello <lucadibello@proton.me>
2026-09-23 22:29:57 +0200

    feat: added `zellij-autolock` and `zellij-forgot` plugins + configured autolock triggers correctly

--- home/zellij.nix
+++ home/zellij.nix
@@ -1,4 +1,15 @@
 { pkgs, ... }:
+let
+  zellij-forgot = pkgs.fetchurl {
+    url = "https://github.com/karimould/zellij-forgot/releases/download/0.4.2/zellij_forgot.wasm";
+    hash = "sha256-MRlBRVGdvcEoaFtFb5cDdDePoZ/J2nQvvkoyG6zkSds=";
+  };
+
+  zellij-autolock = pkgs.fetchurl {
+    url = "https://github.com/fresh2dev/zellij-autolock/releases/download/0.2.2/zellij-autolock.wasm";
+    hash = "sha256-aclWB7/ZfgddZ2KkT9vHA6gqPEkJ27vkOVLwIEh7jqQ=";
+  };
+in
 {
   programs.zellij = {
     enable = true;
@@ -40,6 +51,61 @@
       ui {
           pane_frames {
               rounded_corners true
+          }
+      }
+
+      plugins {
+          autolock location="file:${zellij-autolock}" {
+              is_enabled true
+              triggers "nvim|vim|git|fzf|zoxide|atuin|agy|yazi|lazygit|lazydocker|btop|opencode|llmfit|mole|mo"
+              reaction_seconds "0.3"
+              print_to_log false
+          }
+          forgot location="file:${zellij-forgot}"
+      }
+
+      load_plugins {
+          autolock
+      }
+
+      keybinds {
+          unbind "Ctrl h" "Ctrl l" // nvim keys
+
+          normal {
+              bind "Enter" {
+                  WriteChars "\u{000D}";
+                  MessagePlugin "autolock" {};
+              }
+          }
+
+          locked {
+              bind "Alt z" {
+                  MessagePlugin "autolock" { payload "disable"; };
+                  SwitchToMode "Normal";
+              }
+          }
+
+          shared {
+              bind "Alt Shift z" {
+                  MessagePlugin "autolock" { payload "enable"; };
+              }
+          }
+
+          shared_except "locked" {
+              bind "Ctrl y" {
+                  LaunchOrFocusPlugin "forgot" {
+                      "LOAD_ZELLIJ_BINDINGS" "true"
+                      floating true
+                  }
+              }
+              bind "Alt z" {
+                  MessagePlugin "autolock" { payload "disable"; };
+                  SwitchToMode "Locked";
+              }
+          }
+
+          shared_except "move" "locked" {
+              bind "Ctrl m" { SwitchToMode "Move"; }
           }
       }
     '';
